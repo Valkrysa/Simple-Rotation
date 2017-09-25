266ab72d8e1f64010b9799fce137dc231af40f94
Alice Robinson <valkrysa@gmail.com>
2017-09-25 00:02:56 -0700

    Torque stuff

---
+++ Assets/Scripts/ShowStats.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class ShowStats : MonoBehaviour {
+
+    private Rigidbody rigidBody;
+
+	// Use this for initialization
+	void OnEnable () {
+        rigidBody = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Debug.Log(name + " inertia tensor " + rigidBody.inertiaTensor);
+        Debug.Log(name + " COM " + rigidBody.centerOfMass);
+	}
+}
+
+/*
+1. With no net force, velocity remains constant.
+2. Force = mass * acceleration
+3. Every force has an equal and opposite force.
+
+1. With no net torque, angular velocity remains constant.
+2. Torque = inertia tensor * angular acceleration
+3. Every torque has an equal and opposite torque.
+*/
---
+++ Assets/Scripts/Torque.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Torque : MonoBehaviour {
+
+    public Vector3 torque;
+    public float torqueTime;
+
+    private Rigidbody rigidBody;
+
+    void OnEnable () {
+        rigidBody = GetComponent<Rigidbody>();
+    }
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (torqueTime >= 0f) {
+            rigidBody.AddTorque(torque);
+            torqueTime -= Time.deltaTime;
+        }
+	}
+}
